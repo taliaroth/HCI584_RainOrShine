682c75940479b9f7ec6e91f2a2203a0130b2cca5
taliaroth <59403502+taliaroth@users.noreply.github.com>
2023-06-28 22:12:29 -0400

    Update app.py
    add in forecast route starter code

--- RainOrShine/app.py
+++ RainOrShine/app.py
@@ -18,6 +18,13 @@ url = 'https://api.open-meteo.com/v1/forecast'
 def home():
     return render_template('base.html')
 
+"""" TO DO: finish creating route for base.html to link to -
+@app.route('/forecast', methods=['POST'])
+def forecast():
+    city = request.form.get('city')
+    lat, long = get_cord(city)
+"""
+
 if __name__ == '__main__':
     # run on the local ip at the port number
     app.run(debug=True,port=5001)
