8f4b70743f9147226ef92a1bff3521a226bfd570
taliaroth <59403502+taliaroth@users.noreply.github.com>
2023-07-12 19:11:06 -0400

    Added Autocomplete and Updated Styles
    Used Geoapify to have autocomplete in the search box. Updated the layout of the page using CSS.

--- RainOrShine/app.py
+++ RainOrShine/app.py
@@ -7,9 +7,13 @@ from opencage.geocoder import OpenCageGeocode
 # set flask app variable
 app = Flask(__name__)
 
-# read api key from text file
+# read geocode api key from text file
 with open('api_key.txt', 'r') as file:
     api_key = file.read().strip()
+# read autocomplete api key
+with open('api_auto_complete.txt', 'r') as file:
+    api_key_auto = file.read().strip()
+
 
 # import geocoder for weather
 geocoder = OpenCageGeocode(api_key)
@@ -17,11 +21,36 @@ url = 'https://api.open-meteo.com/v1/forecast'
 
 
 # route flask app to default base.html as homepage
+# TO DO: Add zipcode or if location allowed auto locate
 @app.route('/')
 def home():
     return render_template('base.html')
 
+@app.route('/autocomplete_geocode', methods=['GET'])
+def autocomplete():
+    #get current user input
+    text = request.args.get('text')
 
+    # set up url
+    url = "https://api.geoapify.com/v1/geocode/autocomplete"
+
+    # add api key
+    params = {
+        "text": text,
+        "apiKey": api_key_auto
+    }
+    # create header
+    headers = {
+        "Accept": "application/json"
+    }
+    response = requests.get(url, headers=headers, params=params)
+
+    if response.status_code == 200:
+        return response.json()['features']
+
+        #TO DO: add an error message if city not found - error handling
+
+# add day and am/pm
 # forecast flask route
 @app.route('/forecast', methods=['POST'])
 def forecast():
@@ -44,9 +73,15 @@ def forecast():
             else:
                 image = 'sun.png'
 
+            # get the date and day of the week
+            date_str = json_data['daily']['time'][i]
+            date_obj = datetime.strptime(date_str, '%Y-%m-%d')  # change the format here
+            day_of_week = date_obj.strftime('%A')
+
             # create row using response json data
             row = [
-                json_data['daily']['time'][i],  # get time
+                date_str,  # get time
+                day_of_week,  # get day of the week
                 json_data['daily']['temperature_2m_max'][i],  # get temp max for day - TODO: Get max and min
                 json_data['daily']['precipitation_probability_mean'][i],  # get rain chances
                 image
@@ -104,7 +139,7 @@ def get_forecast_hourly(lat, long, forecast_d=1):  # creating an api call for us
     response = requests.get(url, params=params)
     return response.json()
 
-
+# add next 24 hours and day,am/pm
 @app.route('/hourly_forecast', methods=['POST'])
 def hourly_forecast():
 
@@ -114,7 +149,7 @@ def hourly_forecast():
     # get lat and long
     lat, long = get_cord(city)
     if lat and long:
-        json_data = get_forecast_hourly(lat, long)
+        json_data = get_forecast_hourly(lat, long,2) # use 2 days now to get next 24 hours
 
         # create data needed for forecast
         data = []
@@ -133,8 +168,13 @@ def hourly_forecast():
                     image = 'rain.png'
                 else:
                     image = 'sun.png'
+
+                # get day and get am/pm
+                time_am_pm = forecast_time.strftime('%I:%M %p')
+                day_str = forecast_time.strftime('%A')
                 row = [
-                    json_data['hourly']['time'][i],
+                    time_am_pm,
+                    day_str,
                     json_data['hourly']['temperature_2m'][i],
                     json_data['hourly']['precipitation'][i],
                     image
@@ -142,6 +182,10 @@ def hourly_forecast():
 
                 # add in data
                 data.append(row)
+
+                # break loop after 24 hours
+                if len(data) >= 24:
+                    break
 
                 # render correct template and pass data to html
         return render_template('hourly_forecast.html', city=city, data=data)
